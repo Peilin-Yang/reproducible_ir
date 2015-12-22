737a16065d42092ff6b632efc653b59dcbb6483f
Peilin Yang <yangpeilyn@gmail.com>
2015-12-22 15:23:04 -0500

    plots change

--- src/g.py
+++ src/g.py
@@ -6,48 +6,47 @@ query = [
     "query_class": query.Query,
     "cnt": 50,
     "qf_parts": ['title'],
-    "qrel_program": 'trec_eval -m all_trec -q'
+    "qrel_program": 'trec_eval -m all_trec -q',
+    "main_metric": "MAP"
   },
   {
     "collection": "disk45",
     "query_class": query.Query,
     "cnt": 250,
     "qf_parts": ['title'],
-    "qrel_program": 'trec_eval -m all_trec -q'
+    "qrel_program": 'trec_eval -m all_trec -q',
+    "main_metric": "MAP"
   },  
   {
     "collection": "disk12",
     "query_class": query.Query,
     "cnt": 150,
     "qf_parts": ['title'],
-    "qrel_program": 'trec_eval -m all_trec -q'
+    "qrel_program": 'trec_eval -m all_trec -q',
+    "main_metric": "MAP"
   },   
   {
     "collection": "gov2",
     "query_class": query.Query,
     "cnt": 150,
     "qf_parts": ['title'],
-    "qrel_program": 'trec_eval -m all_trec -q'
-  },  
-  {
-    "collection": "doe",
-    "query_class": query.Query,
-    "cnt": 150,
-    "qf_parts": ['title'],
-    "qrel_program": 'trec_eval -m all_trec -q'
+    "qrel_program": 'trec_eval -m all_trec -q',
+    "main_metric": "MAP"
   },
   {
     "collection": "clueweb09",
     "query_class": query.ClueWebQuery,
     "cnt": 200,
     "qf_parts": ['title'],
-    "qrel_program": 'gdeval.pl'
+    "qrel_program": 'gdeval.pl',
+    "main_metric": "ERR@20"
   },
   {
     "collection": "clueweb12",
     "query_class": query.ClueWebQuery,
     "cnt": 100,
     "qf_parts": ['title'],
-    "qrel_program": 'gdeval.pl'
+    "qrel_program": 'gdeval.pl',
+    "main_metric": "ERR@20"
   }
 ]
--- src/plots.py
+++ src/plots.py
@@ -82,7 +82,7 @@ class Plots(object):
         num_cols = 2
         num_rows = int(math.ceil(len(self.collection_paths)/num_cols))
         size = 4
-        fig, axs = plt.subplots(nrows=num_rows, ncols=num_cols, sharex=False, 
+        fig, axs = plt.subplots(nrows=num_rows, ncols=num_cols, sharex=True, 
             sharey=False, figsize=(size*num_cols, size*num_rows))
         font = {'size' : 16}
         plt.rc('font', **font)
