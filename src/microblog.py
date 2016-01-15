94ed122475adc3fdf799006c5268eb5d9289cf3a
Peilin Yang <yangpeilyn@gmail.com>
2016-01-15 13:37:31 -0500

    add microblog support

--- src/microblog.py
+++ src/microblog.py
@@ -18,6 +18,34 @@ import numpy as np
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+
+def exponetial(x, _lambda=0.1):
+    return np.exp((-1)*x*_lambda)
+
+def log_normal(diff, _mu=1.0, _lambda=1):
+    x = np.divide(np.add(np.log(diff), -_mu), _lambda)
+    _sum = np.copy(x)
+    _values = np.copy(x)
+    for i in range(1, 100):
+        _values = _values*x*x/(2*i+1)
+        _sum += _values
+    phi = 0.5+(_sum/math.sqrt(2*math.pi))*np.exp(x*x/(-2.0))
+    return 1 - phi
+
+def log_logistic(diff, _mu=1.0, _lambda=1):
+    A = np.exp(_mu/_lambda)
+    return A/(A+np.power(diff, 1.0/_lambda))
+
+def linear_cal(x):
+    if x <=0:
+        return 0
+    return x
+
+def linear(diff, _slope=-1.0, _intercept=0.5):
+    A = diff*_slope+_intercept
+    vfunc = np.vectorize(linear_cal)
+    return vfunc(A)
+
 class MicroBlog(object):
     """
     MicroBlog Related
@@ -42,34 +70,6 @@ class MicroBlog(object):
         self.parsed_query_file_path = os.path.join(self.corpus_path, 'parsed_topics.json')
         self.raw_corpus_root = os.path.join(self.corpus_path, 'raw_corpus')
         self.decay_results_root = os.path.join(self.corpus_path, 'decay_results')
-
-
-    def exponetial(x, _lambda=0.1):
-        return np.exp((-1)*x*_lambda)
-
-    def log_normal(diff, _mu=1.0, _lambda=1):
-        x = np.divide(np.add(np.log(diff), -_mu), _lambda)
-        _sum = np.copy(x)
-        _values = np.copy(x)
-        for i in range(1, 100):
-            _values = _values*x*x/(2*i+1)
-            _sum += _values
-        phi = 0.5+(_sum/math.sqrt(2*math.pi))*np.exp(x*x/(-2.0))
-        return 1 - phi
-
-    def log_logistic(diff, _mu=1.0, _lambda=1):
-        A = np.exp(_mu/_lambda)
-        return A/(A+np.power(diff, 1.0/_lambda))
-
-    def linear_cal(x):
-        if x <=0:
-            return 0
-        return x
-
-    def linear(diff, _slope=-1.0, _intercept=0.5):
-        A = diff*_slope+_intercept
-        vfunc = np.vectorize(linear_cal)
-        return vfunc(A)
 
     def gen_run_split_decay_paras(self, methods):
         all_paras = []
