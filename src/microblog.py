0c28995a93cd3d9251914872630c0f44304d9dc8
Peilin Yang <yangpeilyn@gmail.com>
2016-01-15 17:05:50 -0500

    add microblog support

--- src/microblog.py
+++ src/microblog.py
@@ -212,14 +212,9 @@ class MicroBlog(object):
                     min_s = min(this_scores)
                     max_s = max(this_scores)
                     if max_s-min_s != 0:
-                        print min_s, max_s
-                        for did in scores[method][qid]:
-                            if method == 'loglogistic':
-                                print scores[method][qid][did], 
+                        #print min_s, max_s
+                        for did in scores[method][qid]: 
                             scores[method][qid][did] = (scores[method][qid][did]-min_s)/(max_s-min_s)
-                            if method == 'loglogistic':
-                                print scores[method][qid][did]
-                                raw_input()
                             f.write('%s,%s,%f\n' % (qid, did, scores[method][qid][did]))
             
         for ele in itertools.product(funcs['rel'], funcs['decay']):
