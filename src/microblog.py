8dec303362341d178c023ef6c3287e4d6d2ebe73
Peilin Yang <yangpeilyn@gmail.com>
2016-01-15 13:10:30 -0500

    add microblog support

--- src/microblog.py
+++ src/microblog.py
@@ -113,6 +113,7 @@ class MicroBlog(object):
             j = json.load(f)
             for doc in j:
                 doctime = datetime.utcfromtimestamp(float(doc['epoch']))
+                print doctime
                 diffs = relativedelta(querytime, doctime)
         return diffs
 
