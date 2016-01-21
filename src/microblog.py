b20491cdefdf1efef104ef0661777547ca5bd420
Peilin Yang <yangpeilyn@gmail.com>
2016-01-20 19:20:41 -0500

    add microblog support

--- src/microblog.py
+++ src/microblog.py
@@ -231,9 +231,8 @@ class MicroBlog(object):
                             scores[method][qid][did] = (scores[method][qid][did]-min_s)/(max_s-min_s)
                             f.write('%s,%s,%f\n' % (qid, did, scores[method][qid][did]))
         scores = {}
-        for method, path in paths.items():
-            print path
-            with open(path) as f:
+        for method in all_methods:
+            with open( os.path.join(output_folder, method) ) as f:
                 r = csv.reader(f)
                 scores[method] = {}
                 for row in r:
