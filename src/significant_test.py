e0817f1af89491661a3e82f18d79be35e314b665
Peilin Yang <yangpeilyn@gmail.com>
2016-04-21 14:40:21 -0400

    add pairwise ranking models significant test

--- src/significant_test.py
+++ src/significant_test.py
@@ -129,7 +129,7 @@ class SignificantTest(object):
             for ele in itertools.permutations(all_methods, 2):
                 m1_list = [all_results[query_part][ele[0]][k] for k in all_results[query_part][ele[0]] if k in all_results[query_part][ele[1]]]
                 m2_list = [all_results[query_part][ele[1]][k] for k in all_results[query_part][ele[1]] if k in all_results[query_part][ele[0]]]
-                print m1_list, m2_list
+                print ele[0], ele[1]
                 if cal_type == 2:
                     t, p = stats.wilcoxon(m1_list, m2_list)
                 else:
